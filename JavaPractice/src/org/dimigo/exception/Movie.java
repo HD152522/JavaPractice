45f897c43ecfab545a0182e936b5a26cd6a8244a
비밀번호 아는 사람 <  @DESKTOP-EG4QUHK>
2017-05-30 12:30:16 +0900

    .

---
+++ JavaPractice/src/org/dimigo/exception/Movie.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package org.dimigo.exception;
+
+/**
+ * <pre>
+ * org.dimigo.exception
+ *   |_Movie
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 29.
+ * </pre>
+ * 
+ * @author : 비밀번호 아는 사람
+ * @version : 1.0
+ */
+public class Movie {
+	private String title;
+	private int limitAge;
+	
+	public Movie(String title, int limitAge) {
+		this.title = title;
+		this.limitAge = limitAge;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public int getLimitAge() {
+		return limitAge;
+	}
+	
+	public void buyTicket(int age) {
+		try {
+			if(age<this.limitAge) {
+				throw new Exception(this.title + "은/는 " + this.limitAge + "이상 관람가 입니다.");
+			} 
+			System.out.println(this.title + " 즐감하세요.");
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
+}
---
+++ JavaPractice/src/org/dimigo/exception/MovieTest.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.dimigo.exception;
+
+/**
+ * <pre>
+ * org.dimigo.exception
+ *   |_MovieTest
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 30.
+ * </pre>
+ * 
+ * @author : 비밀번호 아는 사람
+ * @version : 1.0
+ */
+public class MovieTest {
+	
+	public static void main(String[] args) {
+		Movie[] movies = {
+				new Movie("에일리언:커버넌트", 15),
+				new Movie("가디언즈 오브 갤럭시", 12)
+				
+		};
+		
+		for(Movie value : movies) value.buyTicket(12);
+
+			
+	}
+	
+	
+}
